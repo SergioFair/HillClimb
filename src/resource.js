4c1f965119006dbfcf26bc48020fd6f63005baa6
SergioFair <sergiomd15@hotmail.com>
2017-12-22 15:41:48 +0100

    Cambiada la imagen del menu. Único avance ya que me desesperé buscando errores durante 1 hora y no encontré absolutamente nada.

--- src/resource.js
+++ src/resource.js
@@ -3,7 +3,7 @@ var res = {
     CloseNormal_png : "res/CloseNormal.png",
     CloseSelected_png : "res/CloseSelected.png",
     boton_jugar_png : "res/boton_jugar.png",
-    menu_titulo_png : "res/menu_titulo.png",
+    menu_titulo_png : "res/hill_climb_menu.jpg",
     tiles32_png: "res/tiles32.png",
     mapa1_tmx: "res/mapa1.tmx",
     mapa2_tmx: "res/mapa2.tmx",
